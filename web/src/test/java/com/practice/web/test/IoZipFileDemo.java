3c0c797a2ddbe45c75dc0d68a36152f5c9f212e2
mark wang <18621740084@163.com>
2022-03-03 21:22:07 +0800

    zipdemo

--- web/src/test/java/com/practice/web/test/IoZipFileDemo.java
+++ web/src/test/java/com/practice/web/test/IoZipFileDemo.java
@@ -2,16 +2,29 @@ package com.practice.web.test;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class IoZipFileDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String aa = "123123123 bububu 你好！   我好！  他好! 啊哈哈ususus   设计师记事本想";
+        byte[] b = aa.getBytes();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ZipOutputStream zos = new ZipOutputStream(baos);
+        ZipEntry entry = new ZipEntry("111.txt");
+        entry.setSize(b.length);
+        zos.putNextEntry(entry);
+        zos.write(b);
+        zos.closeEntry();
+        zos.close();
+
+        System.out.println(Arrays.toString(baos.toByteArray()));
+        new FileOutputStream("/Users/wangfeng/Desktop/test/myFavorites.zip").write(baos.toByteArray());
 
     }
 
@@ -20,8 +33,7 @@ public class IoZipFileDemo {
         try (
                 //指定压缩完成后zip文件的存储路径
                 ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("/Users/wangfeng/Desktop/test/myFavorites.zip"))
-        )
-        {
+        ) {
             //待压缩文件/目录所在的目录
             File fileFolder = new File("/Users/wangfeng/Desktop/test/");
             //获取目录下的所有文件
